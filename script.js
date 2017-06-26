61a5e0456a9e30aca77b477c1d7ea6200f94d20d
Alec Sands <sandsalecv@gmail.com>
2017-06-26 13:42:25 -0500

    final step complete

--- script.js
+++ script.js
@@ -9,8 +9,24 @@ $('document').ready(function() {
     numberOfClicks += 1;
     var $el = $('<div class="red"><p>' + numberOfClicks + '</p></div>');
     $('body').append($el);
-    $el.append('<button type="button" name="Swap">Swap</button>');
-    $el.append('<button type="button" name="Delete">Delete</button>');
+    $el.append('<button type="button" name="Swap" class="swapButton">Swap</button>');
+    $el.append('<button type="button" name="Delete" class ="deleteButton">Delete</button>');
+  });
+
+  $('body').on('click', '.swapButton', function(){
+    var parentDiv = $(this).parent();
+    if (parentDiv.attr('class') == 'red') {
+      parentDiv.removeClass("red");
+      parentDiv.addClass("yellow");
+    } else if (parentDiv.attr('class') == 'yellow') {
+      parentDiv.removeClass("yellow");
+      parentDiv.addClass("red");
+    }
+  });
+
+  $('body').on('click', '.deleteButton', function(){
+    var parentDiv = $(this).parent();
+    parentDiv.remove();
   });
 
 });
